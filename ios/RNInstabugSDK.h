219f3394a81cc3e7d21307cc8a6dc8b9e292357c
Mark Miyashita <negativetwelve@gmail.com>
2016-10-11 04:32:19 -0700

    Add the initial project for RNInstabugSDK on the iOS side

---
+++ ios/RNInstabugSDK.h
@@ -0,0 +1,15 @@
+//
+//  RNInstabugSDK.h
+//  RNInstabugSDK
+//
+//  Created by Mark Miyashita on 10/11/16.
+//  Copyright © 2016 Mark Miyashita. All rights reserved.
+//
+
+
+#import "RCTBridgeModule.h"
+#import "RCTUtils.h"
+
+
+@interface RNInstabugSDK : NSObject <RCTBridgeModule>
+@end
---
+++ ios/RNInstabugSDK.m
@@ -0,0 +1,16 @@
+//
+//  RNInstabugSDK.m
+//  RNInstabugSDK
+//
+//  Created by Mark Miyashita on 10/11/16.
+//  Copyright © 2016 Mark Miyashita. All rights reserved.
+//
+
+
+#import "RNInstabugSDK.h"
+#import <Instabug/Instabug.h>
+
+
+@implementation RNInstabugSDK
+
+@end

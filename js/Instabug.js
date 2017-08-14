675e9e62d4a7a42f4827287e3451ae054b9cb702
Mark Miyashita <negativetwelve@gmail.com>
2017-08-14 03:03:21 -0700

    Change module to use single react-native-package to create the Instabug export

--- js/Instabug.js
+++ js/Instabug.js
@@ -1,28 +1,21 @@
 // Libraries
 import {NativeModules, Platform} from 'react-native';
-import {check} from 'react-native-module-check';
-import {guard} from 'react-native-module-guard';
-import Package from '../package.json';
+import Package from 'react-native-package';
 
 
-// Checks that the module was installed properly. More details at:
-// https://github.com/negativetwelve/react-native-module-check
-check({
-  name: Package.name,
-  repo: Package.homepage,
-  nativeModule: NativeModules.RNInstabugSDK,
-  enabled: Platform.select({
-    ios: true,
-  }),
-});
-
-
-// Guards the module because right now it only works on iOS. When more
-// implementations are added, enabled the platforms below. More details at:
-// https://github.com/negativetwelve/react-native-module-guard
-export default guard({
-  name: Package.name,
-  repo: Package.homepage,
+/**
+ * Package.create handles two things:
+ *
+ *   1. Checks that for each platform that's `enabled`, the module is installed
+ *      properly. If it's not, it logs a warning.
+ *   2. Guards the module on every platform that is not `enabled`. This allows
+ *      the module to exist in cross-platform code without hacks to disable it.
+ *
+ * You can read more about `react-native-package` here:
+ * https://github.com/negativetwelve/react-native-package
+ */
+export default Package.create({
+  json: require('../package.json'),
   nativeModule: NativeModules.RNInstabugSDK,
   enabled: Platform.select({
     ios: true,
